8578e187297d40e4e19f9bb8fd07e84fa0e9a8fa
praneethkumar900 <praneethkumar900@gmail.com>
2017-01-28 16:53:56 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -8,6 +8,11 @@ var app = express()
 //start
 app.post('/webhook', function(request, response)
 {
+  response.writeHead(200, {"Content-Type":"application.json"})
+  var json = JSON.stringify({
+  speech:"yolo",
+    displayText: "erri"
+  })
 console.log("yedava")
 }
 ) 
