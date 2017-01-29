3eb0c6b14375aa3edbdc8f5f3b57df84fa04b379
praneethkumar900 <praneethkumar900@gmail.com>
2017-01-29 12:49:05 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -8,15 +8,49 @@ var app = express()
 //start
 app.post('/webhook', function(request, response)
 {
-  console.log(request);
-  response.writeHead(200, {"Content-Type":"application/json"});
-  var json = JSON.stringify({
-  
-    displayText : "erri",
-    source : "toy"
-  });
-  response.end(json);
-console.log("yedava");
+  console.log("yedava")
+  if(request.body.result.action=="weather")
+  {
+  sendWeather(request,response);
+  }
 }
-) 
+         
+) //app.post
+
+function sendMessage(text, response)
+{
+response.writeHead(200, {"Content-Type":"application/json"})
+  var json = JSON.stringify({
+    speech:text
+    source : "text"
+  })
+  response.end(json)
+}
+
+function sendWeather(request, response)
+{
+  console.log("WEATHER OCCHINDHI")
+city= request.body.result.parameters["geo-city"]
+  request({
+    url:"api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=93e0f7faf62f96d54eb1d5caa28ed417",
+    json:true
+  }, function(error, res, body)
+          {
+    if(body!= null)
+    {
+    if(body.weather !=null)
+    {
+    if(body.weather[0].description!=null)
+    {
+    var weather= "Today, in" +body.city+ " the weather is" +body.weather[0].description+ " and the temperature is " +body.temp
+    sendMessage(weather, response)
+    }
+    
+    }
+      
+    }
+  })
+  
+ 
+}
 app.listen(port)
