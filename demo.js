4ebf3957d9562188d2532dfaf620b6129163e18e
praneethkumar900 <praneethkumar900@gmail.com>
2017-02-11 16:33:20 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -160,7 +160,7 @@ response.writeHead(200, {"Content-Type":"application/json"})
             "title":body.items[2].volumeInfo.title,
             "image_url":body.items[2].volumeInfo.imageLinks[1],
             "subtitle":"author: " + body.items[2].volumeInfo.authors[0]+ ", Category: "  + body.items[2].volumeInfo.categories[0] +", Rating: " + body.items[2].volumeInfo.averageRating
-           },
+           }/*,
           {
             "title":body.items[3].volumeInfo.title,
             "image_url":body.items[3].volumeInfo.imageLinks[1],
