b55d8b0f4f57a4d3eaf1667c705101ec136a7cf5
praneethkumar900 <praneethkumar900@gmail.com>
2017-02-18 18:07:52 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -140,7 +140,7 @@ response.writeHead(200, {"Content-Type":"application/json"})
   var i=0;
     body.items.forEach ( function(ink) {
     
-    if(ink.volumeInfo.authors!= null && i< 10 )
+    if(ink.volumeInfo.authors!= null && i< 8 )
     {
   console.log(ink.volumeInfo.title+" "+ ink.volumeInfo.authors)
   inko.push({
