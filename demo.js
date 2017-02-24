22214506dcef1d184254374f81205f7fe2773321
praneethkumar900 <praneethkumar900@gmail.com>
2017-02-24 18:04:35 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -154,7 +154,7 @@ word_query = req.body.result.resolvedQuery
     //body: formData,
     //method: 'POST'
   }, function (err, res, body) {
-    console.log(body);
+    console.log(body.results);
    /* var word_description = "Word: "+body.results.id+" "+body.results.lexicalEntries.pronunciations.phoneticSpelling+"\\n"+
                             "( "+body.results.lexicalEntries.lexicalCategory+" )\\n"+
 
