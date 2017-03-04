ecc57510e932399b49696696d06a39e7b5b77b19
praneethkumar900 <praneethkumar900@gmail.com>
2017-03-04 13:30:05 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -169,8 +169,25 @@ word_query = req.body.result.resolvedQuery
 
 function sendQuick(text, response)
 {
+  response.writeHead(200, {"Content-Type":"application/json"}
  var json = JSON.stringify({
-    speech:text,
+    data:{
+  "facebook": {
+    "text":"Pick a color:",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Red",
+        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+      },
+      {
+        "content_type":"text",
+        "title":"Green",
+        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+      }
+    ]
+  }
+},
     source : "text"
   })
   response.end(json)
