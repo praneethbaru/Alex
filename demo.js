2c7ee737f8af6200dfcccc87e6bcabbba38f58d9
praneethkumar900 <praneethkumar900@gmail.com>
2017-03-24 12:30:19 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -174,17 +174,20 @@ function sendReceiptMessage(r_query, json, request, response)
 response.writeHead(200, {"Content-Type":"application/json"})
   var json = JSON.stringify({
    data:{
-   "facebook": {
-    "attachment": {
-      "type": "template",
-      "payload": {
-      "template_type":"receipt",
-         "recipient_name":"Stephane Crozatier",
-        "order_number":"123456789",
-        "currency":"INR",
-        "payment_method":"PayTm karo",
+   
+  "facebook": {
+   "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"receipt",
+        "recipient_name":"Stephane Crozatier",
+        "order_number":"12345678902",
+        "currency":"USD",
+        "payment_method":"Visa 2345",        
+        "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
+        "timestamp":"1428444852", 
         "elements": inko,
-       "address":{
+        "address":{
           "street_1":"1 Hacker Way",
           "street_2":"",
           "city":"Menlo Park",
@@ -192,15 +195,25 @@ response.writeHead(200, {"Content-Type":"application/json"})
           "state":"CA",
           "country":"US"
         },
-         "summary":{
+        "summary":{
           "subtotal":75.00,
           "shipping_cost":4.95,
           "total_tax":6.19,
           "total_cost":56.14
-        }
-      }
+        },
+        "adjustments":[
+          {
+            "name":"New Customer Discount",
+            "amount":20
+          },
+          {
+            "name":"$10 Off Coupon",
+            "amount":10
+          }
+        ]//ads
       }
     }
+  }
    },//data
     source : "text"
   })//json
