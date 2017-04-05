a8e5edcc727b574e5b40eeae48f9590bfbfaede9
praneethkumar900 <praneethkumar900@gmail.com>
2017-04-06 00:21:14 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -375,7 +375,7 @@ function sendNews(req, response)
   else
   {
     var source = "bbc-news"
-    }
+    
     if(news_query==" general")
       source = "the-hindu"
     
@@ -406,7 +406,7 @@ function sendNews(req, response)
            console.log(error)
   }
          )
-  //}//else
+  }//else
       console.log("Bahubali 22222!!!!")  
 }//main function
 
